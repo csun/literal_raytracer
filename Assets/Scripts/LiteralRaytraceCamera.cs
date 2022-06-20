621723eac516ff0d8db50df42e1c4d9017434ffb
Cameron Sun <me@csun.io>
2022-06-19 19:07:49 -0700

    Rendertexture displaying

--- Assets/Scripts/LiteralRaytraceCamera.cs
+++ Assets/Scripts/LiteralRaytraceCamera.cs
@@ -6,41 +6,34 @@ namespace LiteralRaytrace
     public class LiteralRaytraceCamera : MonoBehaviour
     {
         public ComputeShader RayTracingShader;
-        private RenderTexture _target;
+        [HideInInspector]
+        public RenderTexture Target;
 
-        private void OnRenderImage(RenderTexture source, RenderTexture destination)
-        {
-            Render(destination);
-        }
-
-        private void Render(RenderTexture destination)
+        private void Update()
         {
             // Make sure we have a current render target
             InitRenderTexture();
 
             // Set the target and dispatch the compute shader
-            RayTracingShader.SetTexture(0, "Result", _target);
+            RayTracingShader.SetTexture(0, "Result", Target);
             int threadGroupsX = Mathf.CeilToInt(Screen.width / 8.0f);
             int threadGroupsY = Mathf.CeilToInt(Screen.height / 8.0f);
             RayTracingShader.Dispatch(0, threadGroupsX, threadGroupsY, 1);
-
-            // Blit the result texture to the screen
-            Graphics.Blit(_target, destination);
         }
 
         private void InitRenderTexture()
         {
-            if (_target == null || _target.width != Screen.width || _target.height != Screen.height)
+            if (Target == null || Target.width != Screen.width || Target.height != Screen.height)
             {
                 // Release render texture if we already have one
-                if (_target != null)
-                    _target.Release();
+                if (Target != null)
+                    Target.Release();
 
                 // Get a render target for Ray Tracing
-                _target = new RenderTexture(Screen.width, Screen.height, 0,
+                Target = new RenderTexture(Screen.width, Screen.height, 0,
                     RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
-                _target.enableRandomWrite = true;
-                _target.Create();
+                Target.enableRandomWrite = true;
+                Target.Create();
             }
         }
     }
---
+++ Assets/Scripts/LiteralRaytraceOutputDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace LiteralRaytrace
+{
+    public class LiteralRaytraceOutputDisplay : MonoBehaviour
+    {
+        public RawImage Image;
+        public LiteralRaytraceCamera Camera;
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Image.texture != Camera.Target)
+            {
+                Image.texture = Camera.Target;
+                Image.SetNativeSize();
+            }
+        }
+    }
+}

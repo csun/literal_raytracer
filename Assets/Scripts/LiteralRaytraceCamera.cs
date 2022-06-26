6f224d3351ab9b46237af6ed03ba6a26e7c3636f
Cameron Sun <me@csun.io>
2022-06-26 16:49:06 -0700

    It's doing something...

--- Assets/Scripts/LiteralRaytraceCamera.cs
+++ Assets/Scripts/LiteralRaytraceCamera.cs
@@ -14,6 +14,14 @@ namespace LiteralRaytrace
         public int bounces = 0;
     }
 
+    public class DrawRay
+    {
+        public Vector3 screenspaceStart;
+        public Vector3 screenspaceDelta;
+        public float normalizedStartIntensity;
+        public Color color;
+    }
+
     public class CachedMaterial
     {
         public Texture2D albedo;
@@ -23,23 +31,17 @@ namespace LiteralRaytrace
     {
         static ProfilerMarker newRayPerfMarker = new ProfilerMarker("LiteralRaytrace.CreateLightRays");
         static ProfilerMarker processRayPerfMarker = new ProfilerMarker("LiteralRaytrace.ProcessQueuedRays");
-        static ProfilerMarker updateTargetPerfMarker = new ProfilerMarker("LiteralRaytrace.UpdateTarget");
-
-        public Material FullscreenPassMaterial;
-        [HideInInspector]
-        public RenderTexture Target;
 
         public int ActiveRayTarget = 100;
         public int MinBounces = 1;
         public int MaxBounces = 16;
+        [HideInInspector]
+        public List<DrawRay> RaysToDraw = new List<DrawRay>();
 
         public float IntensityUpperBound = EVToIntensity(16);
         public float IntensityLowerBound = EVToIntensity(1);
 
-        public Texture2D averageColor;
-        private Texture2D samples;
-        private float maxSamples;
-        private Queue<CameraRay> rayQueue = new Queue<CameraRay>();
+        private Queue<CameraRay> castQueue = new Queue<CameraRay>();
         private Dictionary<int, CachedMaterial> materialCache = new Dictionary<int, CachedMaterial>();
 
         private new Camera camera;
@@ -52,40 +54,8 @@ namespace LiteralRaytrace
 
         private void Update()
         {
-            Init();
-
             CreateLightRays();
             ProcessQueuedRays();
-
-            UpdateTarget();
-        }
-        private void Init()
-        {
-            if (Target == null || Target.width != Screen.width || Target.height != Screen.height)
-            {
-                if (Target != null)
-                {
-                    Target.Release();
-                }
-
-                Target = new RenderTexture(Screen.width, Screen.height, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
-                Target.enableRandomWrite = true;
-                Target.Create();
-
-                averageColor = new Texture2D(Screen.width, Screen.height, TextureFormat.RGBAFloat, false, true);
-                samples = new Texture2D(Screen.width, Screen.height, TextureFormat.RFloat, false, true);
-                maxSamples = 1;  // Initialize to 1 to avoid divide by zero errors early on
-
-                // Clear texture
-                for (int x = 0; x < Target.width; x++)
-                {
-                    for (int y = 0; y < Target.height; y++)
-                    {
-                        averageColor.SetPixel(x, y, Color.black);
-                        samples.SetPixel(x, y, Color.black);
-                    }
-                }
-            }
         }
 
         private void CreateLightRays()
@@ -100,7 +70,7 @@ namespace LiteralRaytrace
                         var randRotation = Quaternion.AngleAxis(Random.Range(0, 360), light.transform.forward);
                         randRotation *= Quaternion.AngleAxis(Random.Range(0, light.spotAngle / 2), light.transform.up);
 
-                        rayQueue.Enqueue(new CameraRay
+                        castQueue.Enqueue(new CameraRay
                         {
                             start = light.transform.position,
                             direction = randRotation * light.transform.forward,
@@ -116,11 +86,12 @@ namespace LiteralRaytrace
         {
             using (processRayPerfMarker.Auto())
             {
-                var raysToProcess = rayQueue;
-                rayQueue = new Queue<CameraRay>();
-                while (raysToProcess.Count > 0)
+                var raysToProcess = castQueue.Count;
+                RaysToDraw.Clear();
+
+                for (var i = 0; i < raysToProcess; i++)
                 {
-                    var ray = raysToProcess.Dequeue();
+                    var ray = castQueue.Dequeue();
 
                     var maxDistance = InvAttenuation(IntensityLowerBound / ray.startIntensity);
                     RaycastHit hitinfo;
@@ -134,7 +105,15 @@ namespace LiteralRaytrace
                     // Draw the ray if it's past the min bounce threshold
                     if (ray.bounces >= MinBounces)
                     {
-                        RasterizeRay(ray, rayEnd);
+                        var screenspaceStart = camera.WorldToScreenPoint(ray.start);
+
+                        RaysToDraw.Add(new DrawRay
+                        {
+                            screenspaceStart = screenspaceStart,
+                            screenspaceDelta = camera.WorldToScreenPoint(rayEnd) - screenspaceStart,
+                            normalizedStartIntensity = NormalizeIntensity(ray.startIntensity),
+                            color = ray.color
+                        });
                     }
 
                     // Finally, queue a new ray if it will bounce
@@ -148,7 +127,7 @@ namespace LiteralRaytrace
 
                         // TODO take normal map at point into account
 
-                        rayQueue.Enqueue(new CameraRay
+                        castQueue.Enqueue(new CameraRay
                         {
                             start = hitinfo.point,
                             direction = reflectedDirection,
@@ -158,26 +137,6 @@ namespace LiteralRaytrace
                         });
                     }
                 }
-            }
-        }
-
-        private void UpdateTarget()
-        {
-            using (updateTargetPerfMarker.Auto())
-            {
-                averageColor.Apply();
-                samples.Apply();
-
-                FullscreenPassMaterial.SetInt("_RayCount", 3000);
-
-                /*
-                DrawShader.SetTexture(0, "Result", Target);
-                DrawShader.SetTexture(0, "_AverageColor", averageColor);
-                DrawShader.SetTexture(0, "_Samples", samples);
-                DrawShader.SetFloat("_MaxSamples", maxSamples);
-
-                DrawShader.Dispatch(0, Mathf.CeilToInt(Screen.width / 8.0f), Mathf.CeilToInt(Screen.height / 8.0f), 1);
-                */
             }
         }
 
@@ -204,160 +163,6 @@ namespace LiteralRaytrace
             }
 
             return materialCache[id];
-        }
-
-        private void RasterizeRay(CameraRay ray, Vector3 rayEnd)
-        {
-            var screenStart = camera.WorldToScreenPoint(ray.start);
-            var screenEnd = camera.WorldToScreenPoint(rayEnd);
-            var screenDelta = screenEnd - screenStart;
-
-            var startRatio = FindScreenVisibleRatio(screenStart, screenDelta);
-            var endRatio = FindScreenVisibleRatio(screenEnd, -screenDelta);
-
-            if (startRatio > 1 || startRatio < 0 || endRatio > 1 || endRatio < 0)
-            {
-                return;
-            }
-
-            screenStart += screenDelta * startRatio;
-            screenEnd -= screenDelta * endRatio;
-            var totalWorldDistance = (ray.start - rayEnd).magnitude;
-            DrawLine(new Vector2Int(Mathf.FloorToInt(screenStart.x), Mathf.FloorToInt(screenStart.y)),
-                new Vector2Int(Mathf.FloorToInt(screenEnd.x), Mathf.FloorToInt(screenEnd.y)),
-                ray.color,
-                ray.startIntensity,
-                startRatio * totalWorldDistance,
-                totalWorldDistance * (1 - (startRatio + endRatio)));
-
-            // TODO eventually add depth checks
-        }
-
-        private bool ScreenPointInBounds(Vector3 point)
-        {
-            return point.x >= 0 && point.x < Target.width &&
-                point.y >= 0 && point.y < Target.height &&
-                point.z >= 0;
-        }
-
-        // Find the first section of a screenspace line where it becomes visible, expressed as a ratio
-        // of the line segment before to the line segment after this point.
-        private float FindScreenVisibleRatio(Vector3 start, Vector3 delta)
-        {
-            // If our point is already in bounds, we don't need to move at all, so the ratio is 0
-            if (ScreenPointInBounds(start))
-            {
-                return 0;
-            }
-
-            var minCandidate = Mathf.Infinity;
-
-            void UpdateCandidateIfInBounds(float ratio)
-            {
-                if (ratio >= 0 &&
-                    ratio <= 1 &&
-                    ScreenPointInBounds(start + (ratio * delta)) &&
-                    ratio < minCandidate)
-                {
-                    minCandidate = ratio;
-                }
-            }
-
-            // Screen coordinate system starts in lower left with positive x going right and positive y going up
-
-            // left of frame
-            if (start.x < 0 && delta.x > 0)
-            {
-                UpdateCandidateIfInBounds(-start.x / delta.x);
-            }
-            // right of frame
-            else if (start.x >= Target.width && delta.x < 0)
-            {
-                // Add an extra -1 so that it ends up at the pixel index width - 1
-                UpdateCandidateIfInBounds((Target.width - start.x - 1) / delta.x);
-            }
-
-            // below frame
-            if (start.y < 0 && delta.y > 0)
-            {
-                UpdateCandidateIfInBounds(-start.y / delta.y);
-            }
-            // above frame
-            else if (start.y >= Target.height && delta.y < 0)
-            {
-                // Add an extra -1 so that it ends up at the pixel index height - 1
-                UpdateCandidateIfInBounds((Target.height - start.y - 1) / delta.y);
-            }
-
-            // behind camera
-            if (start.z < 0)
-            {
-                UpdateCandidateIfInBounds(-start.z / delta.z);
-            }
-
-            // Return the lowest ratio that gives us a point on the line that's in bounds
-            return minCandidate;
-        }
-
-        // Adapted from this gist https://gist.github.com/Pyr3z/46884d67641094d6cf353358566db566
-        private void DrawLine(Vector2Int start, Vector2Int end, Color baseColor, float sourceIntensity, float initialWorldDistance, float totalWorldDistance)
-        {
-            var delta = new Vector2Int(System.Math.Abs(end.x - start.x), System.Math.Abs(end.y - start.y));
-            var increment = new Vector2Int((end.x < start.x) ? -1 : 1, (end.y < start.y) ? -1 : 1);
-            var initialScreenDistance = delta.magnitude;
-
-            float baseColorHue, baseColorSat, baseColorVal;
-            Color.RGBToHSV(baseColor, out baseColorHue, out baseColorSat, out baseColorVal);
-
-            Color GetColor(float screenDistance)
-            {
-                var intensity = NormalizeIntensity(
-                    sourceIntensity * Attenuation(initialWorldDistance + ((1 - (screenDistance / initialScreenDistance)) * totalWorldDistance)));
-
-                return Color.HSVToRGB(
-                    baseColorHue,
-                    baseColorSat,
-                    baseColorVal * intensity);
-            }
-
-            int i = delta.x + delta.y;
-            int error = delta.x - delta.y;
-            delta.x *= 2;
-            delta.y *= 2;
-
-            while (i-- > 0)
-            {
-                RecordSample(start.x, start.y, GetColor((start - end).magnitude));
-
-                if (error < 0)
-                {
-                    start.y += increment.y;
-                    error += delta.x;
-                }
-                else
-                {
-                    start.x += increment.x;
-                    error -= delta.y;
-                }
-            }
-
-            RecordSample(end.x, end.y, GetColor(0));
-        }
-
-        private void RecordSample(int x, int y, Color color)
-        {
-            var count = samples.GetPixel(x, y).r + 1;
-            samples.SetPixel(x, y, new Color(count, 0, 0));
-            maxSamples = count > maxSamples ? count : maxSamples;
-
-            if (count > 1)
-            {
-                // Average the color in-place
-                var ratio = (count - 1.0f) / count;
-                color = (ratio * averageColor.GetPixel(x, y)) + ((1 - ratio) * color);
-            }
-
-            averageColor.SetPixel(x, y, color);
         }
 
         private float NormalizeIntensity(float intensity)
--- Assets/Shaders/LiteralRaytraceCustomPass.cs
+++ Assets/Shaders/LiteralRaytraceCustomPass.cs
@@ -1,21 +1,53 @@
 using UnityEngine;
 using UnityEngine.Rendering.HighDefinition;
 using UnityEngine.Rendering;
-using UnityEngine.Experimental.Rendering;
+using LiteralRaytrace;
 
 class LiteralRaytraceCustomPass : CustomPass
 {
+    const int MAX_RAYS = 256;
+
+    public LiteralRaytraceCamera RaytraceCamera;
     public ComputeShader SamplingShader;
-    public Material LiteralRaytraceMaterial;
+    public Material DrawMaterial;
 
     RenderTexture colorAndSamples;
 
+    Vector4[] screenspaceRayStartBuf = new Vector4[MAX_RAYS];
+    Vector4[] screenspaceRayDeltaBuf = new Vector4[MAX_RAYS];
+    Vector4[] rayColorBuf = new Vector4[MAX_RAYS];
+    float[] rayStartIntensitiesBuf = new float[MAX_RAYS];
+
     protected override void Setup(ScriptableRenderContext renderContext, CommandBuffer cmd)
     {
-        ReinitialzeTextures();
+        ReinitializeTextures();
     }
 
-    private void ReinitialzeTextures()
+    protected override void Execute(CustomPassContext ctx)
+    {
+        // Disable in scene view
+        if (ctx.hdCamera.camera.cameraType == CameraType.SceneView)
+        {
+            return;
+        }
+        ReinitializeTextures();
+        LoadRayBuffers();
+
+        SamplingShader.SetTexture(0, "_ColorAndSamples", colorAndSamples);
+        SamplingShader.SetTexture(0, "_Depth", ctx.cameraDepthBuffer);
+        SamplingShader.SetVectorArray("_SSRayStarts", screenspaceRayStartBuf);
+        SamplingShader.SetVectorArray("_SSRayDeltas", screenspaceRayDeltaBuf);
+        SamplingShader.SetVectorArray("_RayColors", rayColorBuf);
+        SamplingShader.SetFloats("_RayNormalizedStartIntensities", rayStartIntensitiesBuf);
+        SamplingShader.SetInt("_RayCount", RaytraceCamera.RaysToDraw.Count);
+        ctx.cmd.DispatchCompute(SamplingShader, 0, Screen.width / 8, Screen.height / 8, 1);
+
+        DrawMaterial.SetTexture("_ColorAndSamples", colorAndSamples);
+        SetRenderTargetAuto(ctx.cmd);
+        CoreUtils.DrawFullScreen(ctx.cmd, DrawMaterial);
+    }
+
+    private void ReinitializeTextures()
     {
         if (colorAndSamples == null || colorAndSamples.width != Screen.width || colorAndSamples.height != Screen.height)
         {
@@ -28,29 +60,23 @@ class LiteralRaytraceCustomPass : CustomPass
         }
     }
 
-    protected override void Execute(CustomPassContext ctx)
-    {
-        ReinitialzeTextures();
-
-        // Disable in scene view
-        if (ctx.hdCamera.camera.cameraType == CameraType.SceneView)
-        {
-            return;
-        }
-
-        SamplingShader.SetTexture(0, "_ColorAndSamples", colorAndSamples);
-        ctx.cmd.DispatchCompute(SamplingShader, 0, Screen.width / 8, Screen.height / 8, 1);
-
-        LiteralRaytraceMaterial.SetTexture("_ColorAndSamples", colorAndSamples);
-        SetRenderTargetAuto(ctx.cmd);
-        CoreUtils.DrawFullScreen(ctx.cmd, LiteralRaytraceMaterial);
-    }
-
     void DestroyTextures()
     {
         if (colorAndSamples != null)
         {
             colorAndSamples.Release();
+        }
+    }
+
+    void LoadRayBuffers()
+    {
+        for (var i = 0; i < RaytraceCamera.RaysToDraw.Count; i++)
+        {
+            var ray = RaytraceCamera.RaysToDraw[i];
+            screenspaceRayStartBuf[i] = ray.screenspaceStart;
+            screenspaceRayDeltaBuf[i] = ray.screenspaceDelta;
+            rayColorBuf[i] = ray.color;
+            rayStartIntensitiesBuf[i] = ray.normalizedStartIntensity;
         }
     }
 
